10c432fc3e66b9d74bc1c8759af0a9e29833c42b
Barış Gün <108459412+bgun7@users.noreply.github.com>
2024-06-12 16:58:07 +0300

    add all dtos

---
+++ SignalR.Dto/SocialMediaDto/CreateSocialMediaDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SignalR.Dto.SocialMediaDto
+{
+    public class CreateSocialMediaDto
+    {
+        public string Title { get; set; }
+        public string Icon { get; set; }
+    }
+}
