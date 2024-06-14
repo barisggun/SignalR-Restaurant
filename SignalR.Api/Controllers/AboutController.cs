090adafe2009548977ba8a336889220ae013d27f
Barış Gün <108459412+bgun7@users.noreply.github.com>
2024-06-14 13:12:37 +0300

    add about api controller

---
+++ SignalR.Api/Controllers/AboutController.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SignalR.Business.Abstract;
+using SignalR.Dto.AboutDto;
+using SignalR.Entity.Entities;
+
+namespace SignalR.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AboutController : ControllerBase
+    {
+        private readonly IAboutService _aboutService;
+
+        public AboutController(IAboutService aboutService)
+        {
+            _aboutService = aboutService;
+        }
+
+        [HttpGet]
+        public IActionResult AboutList()
+        {
+            var values = _aboutService.TGetListAll();
+            return Ok(values);
+        }
+
+        [HttpPost]
+        public IActionResult CreateAbout(CreateAboutDto createAboutDto)
+        {
+            About about = new About()
+            {
+                Title = createAboutDto.Title,
+                Description = createAboutDto.Description,
+                ImageUrl = createAboutDto.ImageUrl
+            };
+            _aboutService.TAdd(about);
+            return Ok("About added");
+        }
+
+        [HttpDelete]
+        public IActionResult DeleteAbout(int id)
+        {
+            var values = _aboutService.TGetByID(id);
+            _aboutService.TDelete(values);
+
+            return Ok("About deleted");
+        }
+
+        [HttpPut]
+        public IActionResult UpdateAbout(UpdateAboutDto updateAboutDto)
+        {
+            About about = new About()
+            {
+                AboutID = updateAboutDto.AboutID,
+                Title = updateAboutDto.Title,
+                Description = updateAboutDto.Description,
+                ImageUrl = updateAboutDto.ImageUrl
+            };
+            _aboutService.TUpdate(about);
+            return Ok("About updated");
+        }
+
+        [HttpGet("GetAbout")]
+        public IActionResult GetAbout(int id)
+        {
+            var value =_aboutService.TGetByID(id);
+            return Ok(value);
+        }
+    }
+}
