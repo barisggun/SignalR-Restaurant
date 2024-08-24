39e10ec02a9532865afac42176d92ee8ab419931
Barış Gün <108459412+bgun7@users.noreply.github.com>
2024-08-24 14:09:15 +0300

    add uilayout controller and component partials

--- SignalR.Api/Hubs/SignalRHub.cs
+++ SignalR.Api/Hubs/SignalRHub.cs
@@ -1,16 +1,63 @@
 ﻿using Microsoft.AspNetCore.SignalR;
+using SignalR.Business.Abstract;
 using SignalR.DataAccess.Concrete;
 
 namespace SignalR.Api.Hubs
 {
 	public class SignalRHub : Hub
 	{
-		SignalRContext context = new SignalRContext();
+		private readonly ICategoryService _categoryService;
+		private readonly IProductService _productService;
+        private readonly IMoneyCaseService _moneyCaseService;
+        private readonly IOrderService _orderService;
+        private readonly IMenuTableService _menuTableService;
 
-		public async Task SendCategoryCount()
+        public SignalRHub(ICategoryService categoryService, IProductService productService, IMoneyCaseService moneyCaseService, IOrderService orderService, IMenuTableService menuTableService)
+        {
+            _categoryService = categoryService;
+            _productService = productService;
+            _moneyCaseService = moneyCaseService;
+            _orderService = orderService;
+            _menuTableService = menuTableService;
+        }
+
+        public async Task SendStatistic()
 		{
-			var value = context.Categories.Count();
+			var value = _categoryService.TCategoryCount();
 			await Clients.All.SendAsync("RecieveCategoryCount", value);
-		}
+
+            var value2 = _productService.TProductCount();
+            await Clients.All.SendAsync("RecieveProductCount", value2);
+
+            var value3 = _categoryService.TActiveCategoryCount();
+            await Clients.All.SendAsync("RecieveActiveCategoryCount", value3);
+
+            var value4 = _categoryService.TPassiveCategoryCount();
+            await Clients.All.SendAsync("RecievePassiveCategoryCount", value4);
+
+            var value5 = _productService.TProductCountByCategoryNameHamburger();
+            await Clients.All.SendAsync("RecieveProductCountByCategoryNameHamburger", value5);
+
+            var value6 = _productService.TProductCountByCategoryNameDrink();
+            await Clients.All.SendAsync("RecieveProductCountByCategoryNameDrink", value6);
+
+            var value7 = _productService.TProductPriceAvg();
+            await Clients.All.SendAsync("RecieveProductPriceAvg", value7.ToString("0.00")+ "₺" );
+
+            var value8 = _productService.TProductNameByMaxPrice();
+            await Clients.All.SendAsync("RecieveProductNameByMaxPrice", value8);
+        }
+
+        public async Task SendProgress()
+        {
+            var value = _moneyCaseService.TTotalMoneyCaseAmount();
+            await Clients.All.SendAsync("RecieveTotalMoneyCaseAmount", value.ToString("0.00" + "₺"));
+
+            var value2 = _orderService.TActiveOrderCount();
+            await Clients.All.SendAsync("RecieveActiveOrderCount", value2);
+
+            var value3 = _menuTableService.TMenuTableCount();
+            await Clients.All.SendAsync("RecieveMenuTableCount", value3);
+        }
 	}
 }
